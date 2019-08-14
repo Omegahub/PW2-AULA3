89dd897316f1591ce8e10e2016447978cc452c9d
Omegahub <Omegahub2019@gmail.com>
2019-08-14 19:46:03 -0300

    revisao_ts

---
+++ impl/revisao.js
@@ -0,0 +1,13 @@
+var nt1 = 6;
+var nt2 = 10;
+var nt3 = 5;
+var media = (nt1 + nt2 + nt3) / 3;
+
+if (media >= 5) 
+{
+    console.log("a media é: " + media + " aprovado");
+}
+else 
+{
+    console.log("a media é " + media + " reprovado");
+}
---
+++ revisao.ts
@@ -0,0 +1,13 @@
+let nt1:number = 6;
+let nt2:number = 10;
+let nt3:number = 5;
+let media: number = (nt1 + nt2 + nt3)/3
+
+if(media >= 5)
+{
+    console.log("a media é: " + media + " aprovado");
+}
+else
+{
+    console.log("a media é " + media + " reprovado")
+}
