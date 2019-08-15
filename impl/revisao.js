f86194ee2ff634fff64f20bab3e5a1f9012a5eff
Omegahub <Omegahub2019@gmail.com>
2019-08-14 22:00:06 -0300

    utilização de funcao seta

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,7 +1,9 @@
 var n1 = 6;
 var n2 = 10;
 var n3 = 5;
-var fmedia = function (nota1, nota2, nota3) {
-    return (nota1 + nota2 + nota3) / 3;
-};
+/*let fmedia = function(nota1:number, nota2:number, nota3:number) :number
+{
+    return (nota1 + nota2 + nota3)/3
+}*/
+var fmedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
 console.log("a media " + fmedia(n1, n2, n3));
--- revisao.ts
+++ revisao.ts
@@ -1,8 +1,13 @@
 let n1:number = 6;
 let n2:number = 10;
 let n3:number = 5;
-let fmedia = function(nota1:number, nota2:number, nota3:number) :number
+
+
+/*let fmedia = function(nota1:number, nota2:number, nota3:number) :number
 {
     return (nota1 + nota2 + nota3)/3
-}
+}*/
+
+
+let fmedia = (nota1:number, nota2:number, nota3:number) => (nota1 + nota2 + nota3)/3
 console.log(`a media ${fmedia(n1,n2,n3)}`)
