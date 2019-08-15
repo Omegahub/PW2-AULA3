b1ef4e91d22c59e069c56901d73c6d2f735696dc
Omegahub <Omegahub2019@gmail.com>
2019-08-14 22:13:02 -0300

    valor padrão função

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,9 +1,12 @@
 var n1 = 6;
 var n2 = 10;
-var n3 = 5;
+//let n3:number = 5;
 /*let fmedia = function(nota1:number, nota2:number, nota3:number) :number
 {
     return (nota1 + nota2 + nota3)/3
 }*/
-var fmedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
-console.log("a media " + fmedia(n1, n2, n3));
+var fmedia = function (nota1, nota2, nota3) {
+    if (nota3 === void 0) { nota3 = 5; }
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("a media " + fmedia(n1, n2));
--- revisao.ts
+++ revisao.ts
@@ -1,6 +1,6 @@
 let n1:number = 6;
 let n2:number = 10;
-let n3:number = 5;
+//let n3:number = 5;
 
 
 /*let fmedia = function(nota1:number, nota2:number, nota3:number) :number
@@ -9,5 +9,5 @@ let n3:number = 5;
 }*/
 
 
-let fmedia = (nota1:number, nota2:number, nota3:number) => (nota1 + nota2 + nota3)/3
-console.log(`a media ${fmedia(n1,n2,n3)}`)
+let fmedia =(nota1:number, nota2:number, nota3:number=5) => (nota1 + nota2 + nota3)/3
+console.log(`a media ${fmedia(n1,n2,)}`)
